488053a1938d40f4f14dd21be882a849fe632e6c
RDU729 <93077317+RDU729@users.noreply.github.com>
2023-01-14 00:50:46 +0200

    added main.py to be run by comand line

--- src/main/java/com/api/imageinterpretor/controller/ImageController.java
+++ src/main/java/com/api/imageinterpretor/controller/ImageController.java
@@ -1,7 +1,9 @@
 package com.api.imageinterpretor.controller;
 
 import com.api.imageinterpretor.service.ImageServiceImpl;
+import com.api.imageinterpretor.service.PredictService;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.poi.ss.formula.functions.T;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.MediaType;
@@ -9,8 +11,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.sound.sampled.LineListener;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -18,6 +24,9 @@ import java.io.InputStream;
 public class ImageController {
     @Autowired
     private ImageServiceImpl imageService;
+
+    @Autowired
+    private PredictService predictService;
 
 
     @PostMapping(value = "/sendPhoto")
@@ -32,5 +41,11 @@ public class ImageController {
     @GetMapping(value = "/hi/")
     public ResponseEntity<String> sayHi() {
         return ResponseEntity.ok("Hi");
+    }
+
+    @PostMapping("/predict")
+    public ResponseEntity<String> predictImage() throws IOException, ExecutionException, InterruptedException {
+        String predict = predictService.predict();
+        return ResponseEntity.ok(predict);
     }
 }
